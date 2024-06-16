0b84545004218ab821252e766a28239b9eb89e2b
Jason Douglas <jason@step.com>
2024-06-16 18:12:13 -0400

    feat(new): Another test

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
-// Testing
+// Testing 2
 AppRegistry.registerComponent(appName, () => App)
